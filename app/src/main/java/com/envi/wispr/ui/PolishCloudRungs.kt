7e28ee61a57d0d03c92233ce8b2e0e91893e91a2
saurabhav88 <saurabhav@gmail.com>
2026-09-23 11:04:19 -0400

    Drop the last two unused Polish imports after a full sweep (#259)

--- app/src/main/java/com/envi/wispr/ui/PolishCloudRungs.kt
+++ app/src/main/java/com/envi/wispr/ui/PolishCloudRungs.kt
@@ -36,7 +36,6 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.key
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
--- app/src/main/java/com/envi/wispr/ui/PolishLocalControls.kt
+++ app/src/main/java/com/envi/wispr/ui/PolishLocalControls.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.FilterChip
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
